bc6590850535a17ebf30453833a45e35ef5685af
Francesc M <francescmm@gmail.com>
2019-12-04 21:16:48 +0100

    Adding LD_LIBRARY_PATH to the build script

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,6 +1,7 @@
 wget -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt5.zip;
 unzip -qq qt5.zip;
 export QTDIR=$PWD/qt5
+export LD_LIBRARY_PATH=$QTDIR/lib/:$LD_LIBRARY_PATH
 export PATH=$QTDIR/bin:$PATH;
 export QT_PLUGIN_PATH=$PWD/qt5/plugins;
 $QTDIR/bin/qmake CONFIG+=release GitQlient.pro
