29b4ea06b097c09eee3c9852b74deef3e2a7560e
Francesc M <francescmm@gmail.com>
2019-12-07 17:41:12 +0100

    Showing contents

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -1,9 +1,10 @@
 wget -O qt5.zip https://github.com/francescmm/ci-utils/releases/download/qt_5.13/qt5.zip;
 unzip -qq qt5.zip;
-export QTDIR=$PWD/qt5
+ls -ls qt5
+ls -ls qt5/bin
+export QTDIR=qt5
 export LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
-export PATH=$QTDIR/bin:LD_LIBRARY_PATH;
+export PATH=$QTDIR/bin;
 export QT_PLUGIN_PATH=$PWD/qt5/plugins;
 qmake CONFIG+=release GitQlient.pro
 make -j 4
-make install INSTALL_ROOT=usr
