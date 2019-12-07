6ec89507bd003403090f5e116bc67de2f0567315
Francesc M <francescmm@gmail.com>
2019-12-07 17:46:40 +0100

    Adding make to the path

--- ci-scripts/linux/build.sh
+++ ci-scripts/linux/build.sh
@@ -4,7 +4,7 @@ ls -ls qt5
 ls -ls qt5/bin
 export QTDIR=qt5
 export LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
-export PATH=$QTDIR/bin;
+export PATH=$QTDIR/bin:/usr/bin;
 export QT_PLUGIN_PATH=$PWD/qt5/plugins;
 qmake CONFIG+=release GitQlient.pro
 make -j 4
